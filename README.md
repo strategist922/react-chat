d4a2a214343cbf42fe106718cf175745f4368133
Harvey Ball <me@harveyball.com>
2015-02-12 12:59:40 +0000

    test(ChatApp): ensure component is rendered correctly
    A simple test to check if the ChatApp component renders as expected.
    
    The tests are also run on [Travis-CI](https://travis-ci.org/).
    
    The tests are only run against v0.10 of nodejs as there is an
    outstanding issue with Jest running on 0.11 and 0.12:
    
    [Issue #205](https://github.com/facebook/jest/issues/205)

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# React Chat
+# React Chat [![Build Status](https://travis-ci.org/thestonefox/react-chat.svg?branch=master)](https://travis-ci.org/thestonefox/react-chat)
+
 An example of a simple web chat client built in [ReactJS](https://github.com/facebook/react)
 
 ## Installation
@@ -18,3 +19,9 @@ To specify the server port at run time:
 `PORT=8888 npm start`
 
 Visit `http://0.0.0.0:3001` in the web browser
+
+## Running Tests
+
+To run the [Jest](https://github.com/facebook/jest) tests just type:
+
+`npm test`
---
+++ __tests__/scripts/components/chat_app-test.js
@@ -0,0 +1,25 @@
+'use strict';
+var React, ChatApp, TestUtils, path, chatAppInstance;
+
+path = '../../../scripts/components/';
+jest.dontMock( path + 'chat_app');
+
+describe('ChatApp', () => {
+  beforeEach(() => {
+    React = require('react/addons');
+    TestUtils = React.addons.TestUtils;
+    ChatApp = require( path + 'chat_app');
+    chatAppInstance = TestUtils.renderIntoDocument(<ChatApp />);
+  });
+
+  afterEach(() => {
+    if(chatAppInstance && chatAppInstance.isMounted()) {
+      React.unmountComponentAtNode(chatAppInstance.getDOMNode().parent);
+    }
+  });
+
+  it('renders a title on the page', () => {
+    var title = TestUtils.findRenderedDOMComponentWithTag(chatAppInstance, 'h1');
+    expect(title.getDOMNode().textContent).toEqual('Chat App');
+  });
+});
