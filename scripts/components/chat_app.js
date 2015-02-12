ae856f67b9f0b3c25953065c79baa371884be5ce
Harvey Ball <me@harveyball.com>
2015-02-12 12:59:30 +0000

    feat(ChatApp): use React to render default page content
    ReactJS is now being used to build the body content for the website
    utilising a `ReactClass`.
    
    The app will be built out of a collection of components so a new
    `components` directory has been created to store the required
    components.

---
+++ scripts/components/chat_app.js
@@ -0,0 +1,16 @@
+'use strict';
+var React, ChatApp;
+
+React = require('react');
+
+ChatApp = React.createClass({
+  render() {
+    return (
+      <div className="chat-app">
+        <h1>Chat App</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = ChatApp;
---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+'use strict';
+var React, ChatApp;
+
+React = require('react');
+ChatApp = require('./components/chat_app');
+
+React.render(<ChatApp />, document.body);
